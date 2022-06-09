235829f742a3c2ad680edf62d13ab2a15b0065bc
volkan <vlkn38akyz@gmail.com>
2022-06-09 08:56:11 +0200

    flask da for ve if birlikte calistirildi

--- Flask Dersleri/flask-3/app.py
+++ Flask Dersleri/flask-3/app.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 Tags = ["Montag","Dienstag","Mittwoch","Donnerstag","Freitag","Samstag","Sontag"]
 
+
 @app.route("/")
 def index():
     numbers = [1,2,3,4,5,6,7]
@@ -11,8 +12,15 @@ def index():
 
 @app.route("/print")
 def print():
-    
+   
     return render_template("print.html",len = len(Tags), Tags = Tags)
+
+
+@app.route("/forif")
+def forif():
+    sayilar = [-1,-2,3,5,6,8]
+    return render_template("forif.html",len = len(sayilar), sayilar = sayilar)
+
   
 
 if __name__ == "__main__":
